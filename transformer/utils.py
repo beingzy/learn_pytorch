7c40e2a45cc94602fa7911b8af95dd960f9ec158
Yi Zhang <beingzy@gmail.com>
2021-02-23 09:58:39 +0800

    make placeholder util.py for model management

---
+++ transformer/utils.py
@@ -0,0 +1,14 @@
+""" util functions for model managment (save, load, deployment and etc.)
+"""
+
+
+def save_model(model, filepath):
+    """
+    """
+    raise NotImplementedError
+
+
+def load_model(filepath):
+    """
+    """
+    raise NotADirectoryError
