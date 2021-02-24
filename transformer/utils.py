5d7f4fdd646e54251496a0103c70edc681801c05
Yi Zhang <beingzy@gmail.com>
2021-02-24 11:35:13 +0800

    load_model and set the model to evaluation mode by default

--- transformer/utils.py
+++ transformer/utils.py
@@ -13,8 +13,13 @@ def save_model(model, filepath):
         raise TypeError("only `torch.nn.Module` is supported ({})".format(type(model)))
 
 
-def load_model(filepath):
+def load_model(filepath, turn_on_eval_mode=True):
     """
     """
     model = torch.load(filepath)
+    # set dropout and batch normalization layers to evaluation
+    # mode before running inference. Failed to do so, it will
+    # yield inconsistant inference.
+    if turn_on_eval_mode:
+        model.eval()
     return model
